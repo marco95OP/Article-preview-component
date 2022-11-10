51e7e653733cad8c917a9703c756b22246eb95cf
marco95OP <41342819+marco95OP@users.noreply.github.com>
2022-11-09 20:48:19 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//prepare the elements of html
+let share = document.getElementById('js-btn');
+
+//adding the event
+share.addEventListener('click', function(){
+    console.log('hai cliccato');
+})
